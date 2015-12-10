950f0b9b20ab1237a3258f6e2b9c7eec98bd76da
Pontus Freyhult <pontus@freyhult.net>
2015-12-10 12:06:23 +0100

    Fixes for going live.

--- force.py
+++ force.py
@@ -6,18 +6,46 @@ import sys
 import syslog
 import os
 import subprocess
+import time
 
 
+gracetime = 10
 allowedtries = 3
 verifyfunction = lambda x: x.isalnum()
 
 me = os.getlogin()
 url = None
 excluded = []
+gracename = None
+
+remote = None
+
+try:
+    remote = os.environ['SSH_CLIENT']
+    remote = remote[:remote.index(' ')]
+except:
+    pass
+
+try:
+    gracename = os.path.join(os.environ['HOME'], '.gracetimefile')
+except: 
+    pass
+
 
 syslog.openlog("verify2fa")
 
 def allow():
+
+    try:
+        if gracename:
+
+            f = open(gracename, 'w')
+            f.write("%s %f\n" % (remote, time.time()))
+            f.close()
+
+    except:
+        pass
+
     cmd = "/bin/bash -l"
 
     if os.environ.has_key("SSH_ORIGINAL_COMMAND"):
@@ -28,11 +56,17 @@ def allow():
 
 
 for p in sys.argv[1:]:
-    if p[:4] == "url=":
-        url = p[4:]
-    if p[:8] == "exclude=":
-        for q in p[8:].split(","):
+    if p[:6] == "--url=":
+        url = p[6:]
+    if p[:10] == "--exclude=":
+        for q in p[10:].split(","):
             excluded.append(q)
+    if p[:11] == "--maxtries=":
+        allowedtries=int(p[11:])
+    if p[:12] == "--gracetime=":
+        gracetime=int(p[12:])
+
+
    
 if me in excluded:
     syslog.syslog("Skipping 2FA because %s is excluded." % me)
@@ -43,13 +77,29 @@ if not url:
     syslog.syslog("No URL given for verification, bailing out.")
     sys.exit(1)
 
-
+# Check if we're within gracetime.
+try:
+    if gracename:
+        s = os.stat(gracename)
+        # Check time of file
+        if time.time()-s.st_mtime < gracetime*60:
+            line = open(gracename, 'r').read()
+            l = line.index(' ')
+            if line[:l] == remote and time.time()-float(line[l:]) < gracetime*60:
+                allow()
+except:
+    pass
 
 tried = 0
 
 while tried < allowedtries:
     print "Please enter your second factor: "
-    factor = sys.stdin.readline().strip()
+    factor = None
+
+    while not factor:
+      time.sleep(0.2)
+      factor = sys.stdin.readline().strip()
+
     tried += 1
     
     if not factor:
